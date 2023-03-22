d5f7010183f53e67d5b2bf33ce50a972529becde
Tomas Truyols <tomast25@hotmail.com>
2023-03-22 15:46:31 +0100

    Created FullStackApp

---
+++ courseology-backend/src/main/java/com/example/courseology/CourseologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseologyApplication.class, args);
+	}
+
+}
---
+++ courseology-backend/src/test/java/com/example/courseology/CourseologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
