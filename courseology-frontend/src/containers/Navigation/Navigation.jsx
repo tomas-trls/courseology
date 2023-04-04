259e2cbc895c4547c4d5f63426217ff5c4bbf23a
Tomas Truyols <tomast25@hotmail.com>
2023-04-05 01:45:01 +0200

    style:added logo and styled navbar

--- courseology-frontend/src/containers/Navigation/Navigation.jsx
+++ courseology-frontend/src/containers/Navigation/Navigation.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/Courseology.png";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Navigation.scss";
 const Navigation = ({ handleSearch }) => {
   return (
     <nav className="navbar">
+      <img src={logo} className="navbar__logo" alt="logo" />
       <Link to={"/"} className="navbar__link">
         Home
       </Link>
